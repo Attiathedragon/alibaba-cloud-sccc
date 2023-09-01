607fe96cd45a03e7367d91a37d7dddc29e8e8568
Mohamed Attia <85102022+Attiathedragon@users.noreply.github.com>
2023-09-01 04:44:34 +0300

    dd

--- main.tf
+++ main.tf
@@ -3,26 +3,6 @@
 provider "alicloud" {
   access_key = "ALICLOUD_ACCESS_KEY"
   secret_key = "ALICLOUD_SECRET_KEY"
-
-}
-
-data "alicloud_images" "default" {
-  name_regex  = "^ubuntu18^"
-  most_recent = true
-  owners      = "system"
-}
-
-output "image_id" {
-  value = data.alicloud_images.default.images.0.id
-}
-
-resource "alicloud_instance" "web" {
-  image_id             = data.alicloud_images.default.images.0.id
-  instance_type        = "ecs.t6-c2m1"
-  system_disk_category = "cloud_efficiency"
-  security_groups      = ["${alicloud_security_group.default.id}"]
-  instance_name        = "web"
-  vswitch_id           = "vsw-abc12345"
 }
 
 # Create security group

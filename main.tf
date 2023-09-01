d00f682afc334634a214c5dec9641267880a922e
Mohamed Attia <85102022+Attiathedragon@users.noreply.github.com>
2023-09-01 05:20:35 +0300

    dddda

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "alicloud_images" "default" {
 resource "alicloud_instance" "web" {
   image_id             = "${data.alicloud_images.default.images.0.id}"
   internet_charge_type = "PayByBandwidth"
-  instance_type        = "${data.alicloud_instance_types.ecs.i3g.2xlarge.instance_types.0.id}"
+  instance_type        = "${data.alicloud_instance_types.default.instance_types.0.id}"
   system_disk_category = "cloud_efficiency"
   security_groups      = ["${alicloud_security_group.default.id}"]
   instance_name        = "web"

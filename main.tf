50f096d48d11ebd87c9f26997cd0b1abce806608
Mohamed Attia <85102022+Attiathedragon@users.noreply.github.com>
2023-09-01 04:38:45 +0300

    555

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 # Configure the Alicloud Provider
 
 provider "alicloud" {
-
-  region = "ALICLOUD_REGION"
-  skip_region_validation = "true"
   access_key = "ALICLOUD_ACCESS_KEY"
   secret_key = "ALICLOUD_SECRET_KEY"
+  region = "ALICLOUD_REGION"
+  skip_region_validation = "true"
 
 }
 
